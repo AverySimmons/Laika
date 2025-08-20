763f8cc28b4e4084c4098858bf34d8d03eb14f2a
Novali91 <joeyd@firetruk.ca>
2025-08-20 15:07:16 -0700

    Space player movement + guns/bullets

---
+++ 01_Source/03_Space/01_Player/00_Guns/bullet.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+var speed: float = 1250
+var velocity: Vector2
+var lifetime: float = 5
+var life_timer: float = 0
+
+func _physics_process(delta: float) -> void:
+	global_position +=  velocity * delta
+	
+	life_timer += delta
+	if life_timer >= lifetime:
+		despawn()
+	pass
+
+func shoot(position: Vector2) -> void:
+	velocity = speed * position
+	pass
+
+func _on_area_entered(area) -> void:
+	var obstacle = area.owner
+	#if obstacle is Obstacle:
+		#obstacle.take_damage(1)
+		#despawn()
+	pass
+
+func despawn() -> void:
+	queue_free()
+	pass
---
+++ 01_Source/03_Space/01_Player/player_space.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+# Movement Variables ===============================================================================
+var top_speed: Vector2 = Vector2(500, 250)
+var acceleration: Vector2 = top_speed / 0.1
+var reverse_acceleration: Vector2 = top_speed / 0.03
+var idle_friction: Vector2 = top_speed / 0.07
+var movement_vector: Vector2 = Vector2.ZERO
+var base_velocity: Vector2 = Vector2.ZERO
+
+# Guns =============================================================================================
+@onready var left_gun: Node2D = $LeftGun
+@onready var right_gun: Node2D = $RightGun
+var guns: Array
+
+func _ready() -> void:
+	# Adding all of the guns to guns array
+	guns.append(left_gun)
+	guns.append(right_gun)
+	pass
+
+func _physics_process(delta: float) -> void:
+	# Movement part ================================================================================
+	movement_vector = Input.get_vector("left", "right", "up", "down")
+	if movement_vector != Vector2.ZERO:
+		if base_velocity.normalized().dot(movement_vector) < 0.:
+			base_velocity.x = move_toward(base_velocity.x, movement_vector.x * top_speed.x, reverse_acceleration.x * delta)
+			base_velocity.y = move_toward(base_velocity.y, movement_vector.y * top_speed.y, reverse_acceleration.y * delta)
+		else: 
+			base_velocity.x = move_toward(base_velocity.x, movement_vector.x * top_speed.x, acceleration.x * delta)
+			base_velocity.y = move_toward(base_velocity.y, movement_vector.y * top_speed.y, acceleration.y * delta)
+	else:
+		base_velocity.x = move_toward(base_velocity.x, 0, idle_friction.x * delta)
+		base_velocity.y = move_toward(base_velocity.y, 0, idle_friction.y * delta)
+	global_position += base_velocity * delta
+	pass
+
+func handle_click(position: Vector2) -> void:
+	for gun in guns:
+		gun.handle_click(position)
+	pass
